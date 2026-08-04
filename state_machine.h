cbfde1eb395465beb3a5f8f046eddfb567d60d54
vijay1923 <vijaymagadum1923@gmail.com>
2026-08-04 21:05:09 +0530

    Logged machines state and displayed on hmi

--- state_machine.h
+++ state_machine.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include "config.h"
+#include "hmi.h"
 
 // ---- System state machine ----
 typedef enum
@@ -26,15 +27,19 @@ void enter_state(system_state_t new_state)
     {
         case SYS_WAIT_FOR_START:
             Serial.println("STATE -> WAIT_FOR_START (waiting for first scan to synchronize)");
+           hmi.Write_UString((uint16_t)MESSAGE, String("Waiting for first scan"));
             break;
 
         case SYS_INSPECTION_WINDOW:
             printed_count = 0;
             Serial.println("STATE -> INSPECTION_WINDOW (scan/print allowed)");
+            hmi.Write_UString((uint16_t)MESSAGE, String("Inspection Window Open"));
+            hmi_write_number((uint16_t)PRINT_COUNTER, 0);
             break;
 
         case SYS_CYCLE_TIME:
             Serial.println("STATE -> CYCLE_TIME (printing blocked)");
+            hmi.Write_UString((uint16_t)MESSAGE, String("Cycle Running"));
             break;
     }
 }
