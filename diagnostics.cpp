ee7bfdd9b2a316d0f68a6ec6e40c7f54e83a8a29
vijay1923 <vijaymagadum1923@gmail.com>
2026-08-26 17:16:23 +0530

    update:verbose_logging flag

---
+++ diagnostics.cpp
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+#include "diagnostics.h"
+
+// ---- Debug / diagnostics ----
+bool verbose_logging = false;
+unsigned long boot_millis_ref = 0;
+// --------------------------------------------------------------------------
